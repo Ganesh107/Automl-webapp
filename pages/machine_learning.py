c191dd3f67b5009570737fa33f60759d061f8e01
S Ganesh <sganesh371@gmail.com>
2022-05-11 16:06:18 +0530

    ml models added

--- pages/machine_learning.py
+++ pages/machine_learning.py
@@ -1,5 +1,7 @@
+import numpy
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 def app():
     st.title("Build Machine Learning Models")
@@ -9,4 +11,12 @@ def app():
         data = pd.read_csv(uploaded_file)
     
         #Model selection
-        
+        model_name = st.sidebar.selectbox("Select Classifier",("KNN","SVM","Random Forest"))
+        target = st.text_input("Enter target class")
+        if target not in data.columns:
+            st.warning("Enter Correct Column name")
+            st.error("Invalid Column name")
+        else:
+            x = data.drop(columns=target,axis=1)
+            y = data[target]
+            st.write("Number of classes = ",len(np.unique(y)))
